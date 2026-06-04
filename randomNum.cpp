d0e0fae40b49eb06fef5762071e921a4dcf5a7c5
shuban03 <shubanpshetty@gmail.com>
2026-06-04 15:59:49 +0530

    random

--- randomNum.cpp
+++ randomNum.cpp
@@ -4,9 +4,12 @@
 using namespace std;
 
 int main(){
+    srand(time(0)); // Seed the random number generator
+
     for(int i=0;i<11;i++){
     int randomNum = rand() % 100 + 1; // Generate a random number between 1 and 100
     cout <<randomNum << endl;
 }
     return 0;
 }
+
